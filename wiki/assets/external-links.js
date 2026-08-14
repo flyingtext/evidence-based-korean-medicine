e28e48d761e71ac9e41f1bd93500f1d265059076
JiHyeon Yoon <somehowme@gmail.com>
2026-08-15 01:02:11 +0900

    fix: 같은 도메인(내부) 링크는 새 창으로 열지 않도록 수정

--- wiki/assets/external-links.js
+++ wiki/assets/external-links.js
@@ -1,10 +1,11 @@
 (function () {
   function handle() {
+    var origin = location.origin;
     document.querySelectorAll("a[href]").forEach(function (a) {
       var href = a.getAttribute("href") || "";
       var isExternal =
-        href.indexOf("://") !== -1 ||
-        href.indexOf("//") === 0;
+        (href.indexOf("://") !== -1 || href.indexOf("//") === 0) &&
+        href.indexOf(origin) !== 0;
       if (isExternal) {
         a.setAttribute("target", "_blank");
         a.setAttribute("rel", "noopener noreferrer");
